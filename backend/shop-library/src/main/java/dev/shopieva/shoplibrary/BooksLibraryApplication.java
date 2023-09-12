86efb7f181c59ba7a3995ddabfb22a425ba283f0
OlgaShopieva <shopieva94@gmail.com>
2023-09-13 01:52:42 +0300

    added Navigation Bar and images

--- backend/shop-library/src/main/java/dev/shopieva/shoplibrary/BooksLibraryApplication.java
+++ backend/shop-library/src/main/java/dev/shopieva/shoplibrary/BooksLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoplibraryApplication {
+public class BooksLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoplibraryApplication.class, args);
+		SpringApplication.run(BooksLibraryApplication.class, args);
 	}
 
 }
